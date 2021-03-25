d47ebc8338a2702292e81dbf3008dd288f20279e
Rado <rado@dilypse.io>
2021-03-25 14:13:31 +0300

    Grpc done

--- libs/grpc-config/src/index.ts
+++ libs/grpc-config/src/index.ts
@@ -1,3 +1,46 @@
-export * from './grpc-config.module';
-export * from './grpc-config.service';
-export * from './configuration';
+import {
+  ClientProviderOptions,
+  MicroserviceOptions,
+  Transport,
+} from '@nestjs/microservices';
+import { readFileSync } from 'fs';
+import * as yaml from 'js-yaml';
+import { join } from 'path';
+
+const YAML_CONFIG_FILENAME = '../../../config/grpc.yml';
+
+interface PackageConfig {
+  url: string;
+  protoPathServer: string;
+  protoPathClient: string;
+  package: string;
+  name: string;
+}
+export interface GrpcConfiguration {
+  [key: string]: PackageConfig;
+}
+
+export const grpcConfiguration = () => {
+  return yaml.load(
+    readFileSync(join(__dirname, YAML_CONFIG_FILENAME), 'utf8'),
+  ) as Record<string, any>;
+};
+
+export const grpcTransport = (
+  key: string,
+  name?: boolean,
+): MicroserviceOptions | ClientProviderOptions => {
+  const config: PackageConfig = grpcConfiguration()[key];
+  return {
+    ...(name ? { name: config.name } : {}),
+    transport: Transport.GRPC,
+    options: {
+      url: config.url,
+      package: config.package,
+      protoPath: join(
+        __dirname,
+        config[name ? 'protoPathClient' : 'protoPathServer'],
+      ),
+    },
+  };
+};
